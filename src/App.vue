ec95952882f60bfb7e16f699900ac95b11371248
nabuin <nabuin@hiberus.com>
2025-04-29 09:09:51 +0200

    Implementacion del componente header en app.vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/Header.vue';
+
+export default {
+  components: {
+    Header,
+  }
+};
+</script>
+
+<template>
+  <div class="layout">
+    <Header />
+  </div>
+</template>
