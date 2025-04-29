4469561049f30b6052739e623af1cab9ccf5f692
PruebaGit <n.abuin2005@gmail.com>
2025-04-29 18:38:55 +0200

    Implementacion del footer al App.vue, y estilos del layout

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   components: {
     Header,
+    Footer,
   }
 };
 </script>
@@ -12,5 +14,22 @@ export default {
 <template>
   <div class="layout">
     <Header />
+    <RouterLink />
+    <Footer />
   </div>
 </template>
+
+
+<style scoped lang="scss">
+
+.layout {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.layout > *:nth-last-child(2) {
+  flex: 1;
+}
+
+</style>
