30491e1ef41f98140191e65c2ea7f3e06696013c
nabuin <nabuin@hiberus.com>
2025-05-05 13:49:58 +0200

    bloques completamente responsive

--- src/components/Home_Servicios.vue
+++ src/components/Home_Servicios.vue
@@ -1,45 +1,50 @@
 <template>
 
-    <div class="servicios-block">
+  <div class="servicios-block">
 
-        <div class="servicios-block_height-horiz">
+    <div class="servicios-block_height-horiz">
 
-            <div class="servicios-block_height-horiz_block">
-                <div class="servicios-block_height-horiz-text">
-                    <div class="title">Servicios para nuestros usuarios</div>
-                </div>
-                <div class="servicios-block_height-horiz-card">
-                    <div class="icon"><img src="../imgs/service/air.svg" alt=""></div>
-                    <div class="title">Aire</div>
-                    <div class="text">Todas las zonas de nuestras instalaciones disponen de servicios de aire acondicionado para cuando sea necesario</div>
-                </div>
-                <div class="servicios-block_height-horiz-card">
-                    <div class="icon"><img src="../imgs/service/cocina.svg" alt=""></div>
-                    <div class="title">Cocina</div>
-                    <div class="text">Aparte de las zonas de trabajo, les disponemos de una zona de cocina, en la que aparte de prepara y calentar comida, podran pasar tiempo con sus compañeros</div>
-                </div>
-            </div>
-
-            <div class="servicios-block_height-horiz_block">
-                <div class="servicios-block_height-horiz-card">
-                    <div class="icon"><img src="../imgs/service/pcs.svg" alt=""></div>
-                    <div class="title">PCs</div>
-                    <div class="text">En caso de que tu o tu equipo no dispongais de un equipo para poder trabajar, nosotros podemos proporcionarselo</div>
-                </div>
-                <div class="servicios-block_height-horiz-card">
-                    <div class="icon"><img src="../imgs/service/seguridad.svg" alt=""></div>
-                    <div class="title">Seguridad</div>
-                    <div class="text">Disponemos de un equipo de vigilancia las 24 horas del dia, para asegurarnos de que no ocurra ningun altercado desafortunados a nuestros usuarios</div>
-                </div>
-                <div class="servicios-block_height-horiz-card">
-                    <div class="icon"><img src="../imgs/service/wifi.svg" alt=""></div>
-                    <div class="title">Internet</div>
-                    <div class="text">Todas nuestas instalaciones disponen de un servicio a internet siempre que sea requerido, que el maximo nivel de seguridad</div>
-                </div>
-            </div>
+      <div class="servicios-block_height-horiz_block">
+        <div class="servicios-block_height-horiz-text">
+          <div class="title">Servicios para nuestros usuarios</div>
         </div>
+        <div class="servicios-block_height-horiz-card">
+          <div class="icon"><img src="../imgs/service/air.svg" alt=""></div>
+          <div class="title">Aire</div>
+          <div class="text">Todas las zonas de nuestras instalaciones disponen de servicios de aire acondicionado para
+            cuando sea necesario</div>
+        </div>
+        <div class="servicios-block_height-horiz-card">
+          <div class="icon"><img src="../imgs/service/cocina.svg" alt=""></div>
+          <div class="title">Cocina</div>
+          <div class="text">Aparte de las zonas de trabajo, les disponemos de una zona de cocina, en la que aparte de
+            prepara y calentar comida, podran pasar tiempo con sus compañeros</div>
+        </div>
+      </div>
 
+      <div class="servicios-block_height-horiz_block">
+        <div class="servicios-block_height-horiz-card">
+          <div class="icon"><img src="../imgs/service/pcs.svg" alt=""></div>
+          <div class="title">PCs</div>
+          <div class="text">En caso de que tu o tu equipo no dispongais de un equipo para poder trabajar, nosotros
+            podemos proporcionarselo</div>
+        </div>
+        <div class="servicios-block_height-horiz-card">
+          <div class="icon"><img src="../imgs/service/seguridad.svg" alt=""></div>
+          <div class="title">Seguridad</div>
+          <div class="text">Disponemos de un equipo de vigilancia las 24 horas del dia, para asegurarnos de que no
+            ocurra ningun altercado desafortunados a nuestros usuarios</div>
+        </div>
+        <div class="servicios-block_height-horiz-card">
+          <div class="icon"><img src="../imgs/service/wifi.svg" alt=""></div>
+          <div class="title">Internet</div>
+          <div class="text">Todas nuestas instalaciones disponen de un servicio a internet siempre que sea requerido,
+            que el maximo nivel de seguridad</div>
+        </div>
+      </div>
     </div>
+
+  </div>
 
 </template>
 
@@ -48,11 +53,10 @@
   display: flex;
   justify-content: center;
   align-items: center;
-  margin: 25px 0 25px 0;
+  margin: 25px 0;
 
   .servicios-block_height-horiz {
     height: auto;
-    height: 872px;
     width: 702px;
 
     .servicios-block_height-horiz_block {
@@ -89,6 +93,54 @@
 
         .text {
           padding: 10px 10px 10px 25px;
+        }
+      }
+    }
+  }
+}
+
+@media (max-width: 700px) {
+  .servicios-block {
+    .servicios-block_height-horiz {
+      width: 100%;
+      padding: 0 10px;
+
+      .servicios-block_height-horiz_block {
+        flex-direction: column;
+        align-items: center;
+
+        .servicios-block_height-horiz-text {
+          width: 100%;
+          text-align: center;
+          font-size: 30px;
+          margin: 20px 0;
+        }
+
+        .servicios-block_height-horiz-card {
+          width: 100%;
+          margin: 10px 0;
+
+          .icon {
+
+            display: flex !important;
+            justify-content: center !important;
+
+            img {
+              padding: 10px 10px 0 10px;
+              height: 60px;
+            }
+          }
+
+          .title {
+            font-size: 24px;
+            padding: 10px 10px 0 10px;
+            text-align: center;
+          }
+
+          .text {
+            padding: 10px;
+            text-align: center;
+          }
         }
       }
     }
