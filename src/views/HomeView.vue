603449bfc27ea30ae367c1ff0ee140f691e2544d
nabuin <nabuin@hiberus.com>
2025-05-05 11:46:56 +0200

    Implementacion del bloque en la home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script lang="ts">
 import Home_Slider from '../components/Home_Slider.vue';
 import Home_Sercios from '../components/Home_Servicios.vue';
+import Home_Info from '../components/Home_Info.vue'
 
 export default {
   components: {
     Home_Slider,
-    Home_Sercios
+    Home_Sercios,
+    Home_Info
   },
 };
 </script>
@@ -14,6 +16,7 @@ export default {
 <template>
     <Home_Slider />
     <Home_Sercios />
+    <Home_Info />
 </template>
 
 
