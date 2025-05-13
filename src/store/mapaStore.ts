27c58b749438d55b1f295e7ff080fbaade4cc0d2
PruebaGit <n.abuin2005@gmail.com>
2025-05-13 19:05:38 +0200

    Creacion funcional de store para el mapa (sedes)

---
+++ src/store/mapaStore.ts
@@ -0,0 +1,30 @@
+import { ref } from 'vue';
+
+interface Sede {
+  idSede: number;
+  pais: string;
+  ciudad: string;
+  direccion: string;
+  codigoPostal: string;
+  planta: string;
+  urL_Imagen: string;
+  latitud: string;
+  longitud: string;
+  observaciones: string;
+}
+
+export function useMapaStore() {
+  const coordenadas = ref<Sede[]>([]);
+
+  const obtenerCoordenadas = async () => {
+    try {
+      const res = await fetch('https://localhost:7179/api/Sedes');
+      if (!res.ok) throw new Error('Error en la solicitud');
+      coordenadas.value = await res.json();
+    } catch (error) {
+      console.error('Error al obtener sedes:', error);
+    }
+  };
+
+  return { coordenadas, obtenerCoordenadas };
+}
