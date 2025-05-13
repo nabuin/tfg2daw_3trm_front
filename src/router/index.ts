62df5e56252fa0962fed775cd9a024e2635fa53b
PruebaGit <n.abuin2005@gmail.com>
2025-05-13 19:06:18 +0200

    implementacion de url para sedes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '../views/HomeView.vue';
+import SedesView from '../views/SedesView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       path: '/home',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/sedes',
+      name: 'sedes',
+      component: SedesView,
     },
   ],
 });
