5b6041934a67c04a862705a6cd7d11731ed1e6a4
nabuin <nabuin@hiberus.com>
2025-05-15 12:26:27 +0200

    ruta a salas

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '../views/HomeView.vue';
 import SedesView from '../views/SedesView.vue';
+import SalasView from '../views/SalasView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/sedes',
       name: 'sedes',
       component: SedesView,
+    },
+    {
+      path: '/sedes/salas',
+      name: 'salas',
+      component: SalasView,
     },
   ],
 });
