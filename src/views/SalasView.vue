d434c8030a95ec0c9328432495e2aeac24350b3a
nabuin <nabuin@hiberus.com>
2025-05-15 12:28:27 +0200

    view de sedes creado

---
+++ src/views/SalasView.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import Salas_Filter from '../components/Salas_Filter.vue';
+
+export default {
+  components: {
+    Salas_Filter,
+  },
+};
+</script>
+
+
+<template>
+    <Salas_Filter />
+</template>
+
+
+<style scoped lang="scss">
+
+
+</style>
+
