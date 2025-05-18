7e0dded09dd8b268c7bfb22a4b695c86074184e1
PruebaGit <n.abuin2005@gmail.com>
2025-05-18 15:25:46 +0200

    estructura para asientos

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue';
 import SedesView from '../views/SedesView.vue';
 import SalasView from '../views/SalasView.vue';
+import PuestosView from '../views/PuestosView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,11 @@ const router = createRouter({
       path: '/sedes/salas',
       name: 'salas',
       component: SalasView,
+    },
+    {
+      path: '/sedes/salas/puestos',
+      name: 'puestos',
+      component: PuestosView,
     },
   ],
 });
---
+++ src/views/PuestosView.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import Puestos_Mostrar from '../components/Puestos_Mostrar.vue';
+
+export default {
+  components: {
+    Puestos_Mostrar,
+  },
+};
+</script>
+
+
+<template>
+    <Puestos_Mostrar />
+</template>
+
+
+<style scoped lang="scss">
+
+
+</style>
+
