157ee951dfe68f01242e65b33221dc6d485b13e5
nabuin <nabuin@hiberus.com>
2025-06-02 12:51:05 +0200

    Eliminacion de servicios

--- src/views/ServiciosView.vue
+++ src/views/ServiciosView.vue
@@ -1,2 +1,17 @@
-<template></template>
-<script></script>
+<script>
+import { defineComponent } from 'vue';
+import Servicios_Blocks from '../components/Servicios_Blocks.vue';
+
+export default defineComponent({
+    name: 'SedesView',
+    components: {
+        Servicios_Blocks
+    }
+});
+</script>
+
+<template>
+
+<Servicios_Blocks/>
+
+</template>
